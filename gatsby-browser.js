45f2d799ed888c08b6e1a5c040f7bb3aee2eca3a
Michael Esteban <mickel13@gmail.com>
2022-01-31 12:00:36 +1000

    migrate to astro

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { ToggleProvider } from "./src/context/toggle"
-
-export const wrapRootElement = ({ element }) => (
-  <ToggleProvider>{element}</ToggleProvider>
-)
