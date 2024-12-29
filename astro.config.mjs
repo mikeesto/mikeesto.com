30c4aae1dfbdd288fda8e5cc72bbb62dc2b6b595
Michael Esteban <mickel13@gmail.com>
2024-12-29 18:37:34 +1000

    use lucide instead of lucide-react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +12,5 @@ export default defineConfig({
       wrap: true,
     },
   },
-  integrations: [sitemap(), tailwind(), react()],
+  integrations: [sitemap(), tailwind()],
 });
